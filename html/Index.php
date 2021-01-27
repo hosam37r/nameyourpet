38977cbbfb429cfcafa239f17fd335d817651d2e
jphilipps04 <77983262+jphilipps04@users.noreply.github.com>
2021-01-27 09:43:50 -0600

    Update Index.php

--- html/Index.php
+++ html/Index.php
@@ -1,7 +1,14 @@
 <?php
     include_once 'header.php';
 ?>
-
+<div class="petDiv">
+<br>
+        <div class="movingTextContainer" id=petcontainer><h1 style="text-align:center">WHAT TYPE OF PET DO YOU HAVE?</h1>
+        </div>
+        <img class="pet" id="dog" src="https://image.flaticon.com/icons/png/512/91/91544.png" height="500" width="400" style="margin: 100px">
+        <img class="pet" id="cat" src="https://static.thenounproject.com/png/6725-200.png" height="500" width="400" style="margin: 100px">
+        <img class="pet" id="other" src="https://static.thenounproject.com/png/5935-200.png" height="400" width="200" style="margin: 100px">
+   </div>
 
 <?php
     include_once 'footer.php';

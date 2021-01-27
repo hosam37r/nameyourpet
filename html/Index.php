329290754d4b99e4ff005b377dbd9272a9deb4f8
jphilipps04 <77983262+jphilipps04@users.noreply.github.com>
2021-01-27 10:56:50 -0600

    Update Index.php

--- html/Index.php
+++ html/Index.php
@@ -1,14 +1,33 @@
 <?php
     include_once 'header.php';
 ?>
-<div class="petDiv">
-<br>
-        <div class="movingTextContainer" id=petcontainer><h1 style="text-align:center">WHAT TYPE OF PET DO YOU HAVE?</h1>
+ <div class="movingTextContainer" id=petcontainer><h1 style="text-align:center">WHAT TYPE OF PET DO YOU HAVE?</h1>
+ </div>
+   
+    <div align="center">
+    <div>
+        <div class="containerD">
+            <img class="pet" alt="Dog Icon" src="https://image.flaticon.com/icons/png/512/91/91544.png">
+        <div class="overlay overlayLeft">
+            <div class="textD">I have a dog</div>
         </div>
-        <img class="pet" id="dog" src="https://image.flaticon.com/icons/png/512/91/91544.png" height="500" width="400" style="margin: 100px">
-        <img class="pet" id="cat" src="https://static.thenounproject.com/png/6725-200.png" height="500" width="400" style="margin: 100px">
-        <img class="pet" id="other" src="https://static.thenounproject.com/png/5935-200.png" height="400" width="200" style="margin: 100px">
-   </div>
+    </div>
+    <div class="containerD">
+            <img class="pet" alt="Cat Icon" src="https://static.thenounproject.com/png/6725-200.png">
+        <div class="overlay overlayLeft">
+            <div class="textD">I have a cat</div>
+        </div>
+    </div>
+        <div class="containerD">
+            <img class="pet" alt="Other Icon" src="https://static.thenounproject.com/png/26623-200.png">
+        <div class="overlay overlayLeft">
+            <div class="textD">I have something else...</div>
+        </div>
+    </div>
+       
+    </div>
+   
+    </div>
 
 <?php
     include_once 'footer.php';

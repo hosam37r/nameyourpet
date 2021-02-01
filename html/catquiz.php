ec7dff980f3b1c51757c37b9abcbd2f06199ff43
jphilipps04 <77983262+jphilipps04@users.noreply.github.com>
2021-02-01 08:17:56 -0600

    Add files via upload

---
+++ html/catquiz.php
@@ -0,0 +1,8 @@
+<?php
+    include_once 'header.php';
+?>
+    <p>cat quiz coming soon</p>
+
+<?php 
+    include_once 'footer.php';
+?>
---
+++ html/dogquiz.php
@@ -0,0 +1,8 @@
+<?php
+    include_once 'header.php';
+?>
+    <p>dog quiz coming soon</p>
+
+<?php 
+    include_once 'footer.php';
+?>

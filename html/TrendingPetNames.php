3afd31ec675344cd5126635b379820633b215adf
JDSchutz <77743582+JDSchutz@users.noreply.github.com>
2021-02-07 22:36:36 -0600

    Update TrendingPetNames.php

--- html/TrendingPetNames.php
+++ html/TrendingPetNames.php
@@ -4,28 +4,36 @@
 
 <style>
 table, th, td {
-  border: 1px solid black;
     margin-left: auto;
     margin-right: auto;
-    background-color: mintcream;
-    padding-bottom: 20px;
-    padding-left: 50px;
-    text-align: left;
-    width: 900px;
+    background-color: white;
+    padding-bottom: 70px;
+   
+    text-align: center;
+    width: 500px;
 }
     h2 {
         text-align: center;
        font-size: 35px;
-        color: white;
+        color: lightpink;
        
     }
  
     .backgroundImage {
-        background-image: url(doggie.jpg);
+        background-image: url(doggo.jpeg);
         background-repeat: no-repeat;
         background-size: 100% 100%;
         background-attachment: fixed;
         background-position: center;
+    }
+    
+    li {
+        padding: 15px;
+    }
+    
+    .top {
+        border-style: solid;
+        
     }
 </style>
 <div class="backgroundImage">
@@ -39,46 +47,46 @@ table, th, td {
 
 <table>
   <tr>
-    <th>Pet Type</th>
+    <th>Animal</th>
     <th>Male</th>
     <th>Female</th>
    
   </tr>
   <tr>
     <td>Dog</td>
-    <td><li>Tucker</li><li>Dakota</li><li>Jake</li></td>
-    <td><li>Ripley<li>Gia</li><li>Bailey</li></td>
-   
- 
+    <td><li>Tucker</li><li>Dakota</li><li>Jake</li><li>Bobbie</li><li>Pickle</li></td>
+    <td><li>Ripley<li>Gia</li><li>Bailey</li><li>Lola</li><li>Georgia</li></td>
    
   </tr>
+    
   <tr>
     <td>Cat</td>
-      <td><li>Winston</li><li>Leo</li><li>Milo</li></td>
-      <td><li>Cleo</li><li>Missy</li><li>Shadow</li></td>
+      <td><li>Winston</li><li>Leo</li><li>Milo</li><li>Shadow</li><li>Oliver</li></td>
+      <td><li>Cleo</li><li>Missy</li><li>Shadow</li><li>Minnie</li><li>Bella</li></td>
   </tr>
   <tr>
       <td>Snake</td>
-    <td><li>Noodles</li><li>Slinky</li><li>Buttercup</li></td>
-    <td><li>Medusa</li><li>Juniper</li><li>Cherry</li></td>
+    <td><li>Noodles</li><li>Slinky</li><li>Buttercup</li><li>Diablo</li><li>Jafaar</li></td>
+    <td><li>Medusa</li><li>Juniper</li><li>Cherry</li><li>Bindi</li><li>Severus</li></td>
   </tr>
   <tr>
       <td>Bunny</td>
-    <td><li>Thumper</li><li>Oreo</li><li>Whiskers</li></td>
-      <td><li>Snowball</li><li>Rose</li><li>Luna</li></td>
+    <td><li>Thumper</li><li>Oreo</li><li>Whiskers</li><li>Drax</li><li>Midnight</li></td>
+      <td><li>Snowball</li><li>Rose</li><li>Luna</li><li>Gretta</li><li>Binky</li></td>
  
   </tr>
   <tr>
     <td>Fish</td>
-      <td><li>Nemo</li><li>Apollo</li><li>Bubbles</li></td>
-      <td><li>Dori</li><li>Coral</li><li>Penny</li></td>
+      <td><li>Nemo</li><li>Apollo</li><li>Bubbles</li><li>Pickle</li><li>Captain</li></td>
+      <td><li>Dori</li><li>Coral</li><li>Penny</li><li>Goldie</li><li>Jewel</li></td>
    
   </tr>
   <tr>
     <td>Bird</td>
-    <td><li>Mojo</li><li>Rico</li><li>Sky</li></td>
-      <td><li>Tweetie</li><li>Kiwi</li><li>Tiki</li></td>
+    <td><li>Mojo</li><li>Rico</li><li>Sky</li><li>Jay</li><li>Oscar</li></td>
+      <td><li>Tweetie</li><li>Kiwi</li><li>Tiki</li><li>Luna</li><li>Skye</li></td>
   </tr>
+
 </table>
 </div>
 <?php

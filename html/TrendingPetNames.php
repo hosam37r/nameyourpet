dee7be5952310f91a05b0a33c84d6616d97e8619
megkita <77743930+megkita@users.noreply.github.com>
2021-02-21 21:49:23 -0600

    Update TrendingPetNames.php

--- html/TrendingPetNames.php
+++ html/TrendingPetNames.php
@@ -16,48 +16,50 @@
 <body>
     
 <div style = "position:fixed;top:0%;height:6%;left:0%;width:99.8%;background-color:rgb(214, 214, 214);border:solid 1px blue;"><!-- Header  -->
- <img src = "doggoWEBKIT.png" style = "position:relative;float:left;width:5%;height:100%;">
 
-<a href = " "><button class = "headerBut">Home</button></a> <!-- fill in your url to the page -->
-<img src = "kittyWEBKIT.jpg" style = "position:relative;float:left;width:5%;height:100%;">
-    
-<a href = "contactUs.html"><button class = "headerBut">Contact Us</button></a> <!-- fill in your url to the page -->
-    
-<img src = "iguanaWEBKIT.png" style = "position:relative;float:left;width:5%;height:100%;">
-<a href = " "><button class = "headerBut">Index</button></a> <!-- fill in your url to the page -->
-    
-<img src = "doggoWEBKIT.png" style = "position:relative;float:left;width:5%;height:100%;">
-<a href = " "><button class = "headerBut">Trending</button></a> <!-- fill in your url to the page -->
-<img src = "images/fishWEBKIT.png" style = "position:relative;float:left;width:5%;height:100%;">
+    <img src = "doggoWEBKIT.png" style = "position:relative;float:left;width:5%;height:100%;">
+    <a href = " "><button class = "headerBut">Home</button></a> <!-- fill in your url to the page -->
+    <img src = "kittyWEBKIT.jpg" style = "position:relative;float:left;width:5%;height:100%;">
+    <a href = ""><button class = "headerBut">Contact Us</button></a> <!-- fill in your url to the page -->
+    <img src = "iguanaWEBKIT.png" style = "position:relative;float:left;width:5%;height:100%;">
+    <a href = ""><button class = "headerBut">Index</button></a> <!-- fill in your url to the page -->
+    <img src = "doggoWEBKIT.png" style = "position:relative;float:left;width:5%;height:100%;">
+    <a href = ""><button class = "headerBut">Trending</button></a> <!-- fill in your url to the page -->
+    <img src = "fishWEBKIT.png" style = "position:relative;float:left;width:5%;height:100%;">
 </div>
 
-<div id = "lowerNav">
-    <h3 style = "position:absolute;left:1%;top:-50%;color:white;font-family: 'Josefin Sans', sans-serif;">Home ></h3>
-</div>
 
 <div id = "center">
-    <h3 id = "centerPageHeader"> Trending Pet Names in 2021</h3>  <!-- Page Heading Here -->
-    <table>
-    <colgroup>
-    <col class= "blue"/>
-    <col class="pink"/>
-    </colgroup>
+    <h2 id = "centerPageHeader">Trending Pet Names in 2021</h2>  <!-- Page Heading Here -->
     
-  <tr>
-    <th>Animal</th>
-    <th>Male</th>
-    <th>Female</th>
-   
-  </tr>
-  <tr>
-    <td>Dog</td>
-    <td class="blue"><li>Tucker</li><li>Dakota</li><li>Jake</li><li>Bobbie</li><li>Pickle</li></td>
+    <div class= "dog_male">
+    <div class="option_list_male">
+        <div class="option_male">
+            <span><h2 style=" font-family: 'Pacifico', cursive;">Dogs</h2></span>
+            <p>Male Names: </p>
+            <li>Tucker</li>
+            <li>Dakota</li>
+            <li>Jake</li>
+            <li>Bobbie</li>
+            <li>Pickle</li>
+        </div>
+    <div class="dog_female">
+    <div class="option_list_female">
+        <div class="option_female">
+            <span><h2 style=" font-family: 'Pacifico', cursive;">Dogs</h2></span>
+            <p>Female Names: </p>
+            <li>Ripley</li>
+            <li>Gia</li>
+            <li>Bailey</li>
+            <li>Lola</li>
+            <li>Georgia</li>
+        </div>
+        </div>
+        </div>
+        </div>
+    </div>
     
-    <td class="pink"><li>Ripley<li>Gia</li><li>Bailey</li><li>Lola</li><li>Georgia</li></td>
-   
-  </tr>
-    
-  <tr>
+<!-- 
     <td>Cat</td>
       <td class="blue"><li>Winston</li><li>Leo</li><li>Milo</li><li>Shadow</li><li>Oliver</li></td>
       
@@ -95,21 +97,23 @@
 <!-- can put main stuff here, make div larger by editing universal style the id called centerPageHeader  -->
 </div>
 
-<style>
+<style> 
 
-table, th, td {
-    margin-left: auto;
-    margin-right: auto;
-    background-color: white;
-    padding-bottom: 70px;
-   
-    text-align: center;
-    width: 500px;
-}
     h2 {
         text-align: center;
-       font-size: 35px;
-        color: lightpink;
+        font-size: 35px;
+       
+    }
+    
+    #center{
+        position:fixed;
+        left:5%;
+        height: 100%;
+        margin-left: 0;
+        margin-right; 0;
+        font-size:1.5em;
+        
+        <!-- width: 100%; -->
        
     }
  
@@ -121,23 +125,68 @@ table, th, td {
         background-position: center;
     }
     
-    li {
-        padding: 15px;
-    }
-    
     .top {
         border-style: solid;
         
     }
-    
-    .blue{
-        background-color: lightblue;
+
+    .dog {
+       position:absolute;
+       margin:0; 
+       margin-top:0; 
+       margin-left:30px;
+       font-size: 0.8em;
     }
     
-    .pink{
-        background-color: lightpink;
+
+    .option_list_male .option_male {
+    background: aliceblue;
+    border: 1px solid #84c5fe;
+    border-radius: 5px;
+    padding: 8px 15px;
+    margin-bottom: 15px;
+    font-size: 17px;
+    justify-content: space-between;
+    margin-left: 30px;
+    margin-top: 10px;
+    cursor: pointer;
+    font-family: 'Pacifico', cursive; 
+    }
+    
+    .option_list_female .option_female {
+    background: lavenderblush;
+    border: 1px solid #ff69b4;
+    border-radius: 5px;
+    padding: 8px 15px;
+    margin-bottom: 15px;
+    font-size: 17px;
+    justify-content: space-between;
+    margin-left: 30px;
+    margin-top: 10px;
+    cursor: pointer;
+    font-family: 'Pacifico', cursive; 
+    }
+    
+    li{
+        list-style-type: circle;
+    }
+    
+    .option_list_male, option_list_female {
+        padding: 5px;
+        margin: 5px;
+        display: flex;
+        float: left;
     }
 </style>
+<script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+</script>
 <div class="backgroundImage">
 <br>
 <br>
@@ -146,16 +195,6 @@ table, th, td {
 </div>
 <div id="mySidenav" class="sidenav"><!-- Side Nav  -->
     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-
-
-
-
-    <h3>Sample Text</h3>
-
-
-
-
-
   </div>
   
 
@@ -172,14 +211,6 @@ table, th, td {
         <a href = "https://www.twitter.com"> <img src = "tw.jpg" style = "position:absolute;left:61%;top:20%;width:2%;height:20%;"></a>
         <h3 style = "position:absolute;left:50%;bottom:20%;">Share Us!</h3>
 
-<script>
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
-</script>
+    </div>
 </body>
 </html>

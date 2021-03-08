83b2c547e760eca72f06183fc3bf8c7ae8842970
RaymondWhitfield <77745220+RaymondWhitfield@users.noreply.github.com>
2021-03-08 12:56:10 -0600

    Updated Links to external websites

--- html/resources.php
+++ html/resources.php
@@ -35,21 +35,21 @@
         Our organization was founded on the belief that animals are entitled to kind and respectful treatment at the hands of humans and must be protected under the law. Headquartered in New York City, the ASPCA maintains a strong local presence, and with programs that extend our anti-cruelty mission across the country, we are recognized as a national animal welfare organization. We are a privately funded 501(c)(3) not-for-profit corporation, and are proud to boast more than 2 million supporters across the country.
         The ASPCA’s mission, as stated by founder Henry Bergh in 1866, is “to provide effective means for the prevention of cruelty to animals throughout the United States.”</p></div>
     <div class="resourceLocation"><p>New York, NY</p></div>
-    <div class="resourceSite"><p><a href="aspca.org">ASPCA Website</a></p></div>
+    <div class="resourceSite"><p><a href="https://www.aspca.org">ASPCA Website</a></p></div>
 </div>
 <div class="resourceContainer">
     <div class="resourceName"><p>The Shelter Pet Project</p></div>
     <div class="resourceSummary"><p>The Shelter Pet Project is the result of a collaborative effort between two leading animal welfare groups, the Humane Society of the United States and Maddie’s Fund, and the leading producer of public service advertising (PSA) campaigns, The Ad Council.
         Our goal is to make shelters the first place potential adopters turn when looking to get a new pet, ensuring that all healthy and treatable pets find loving homes. We do this by breaking down misconceptions surrounding shelter pets and celebrating the unique bond between every shelter pet and parent.</p></div>
     <div class="resourceLocation"><p>Online Presence Only</p></div>
-    <div class="resourceSite"><p><a href="theshelterpetproject.org">The Shelter Pet Project</a></p></div>
+    <div class="resourceSite"><p><a href="https://www.theshelterpetproject.org">The Shelter Pet Project</a></p></div>
 </div>
 <div class="resourceContainer">
     <div class="resourceName"><p>The Humane Society</p></div>
     <div class="resourceSummary"><p>We fight the big fights to end suffering for all animals.
         Together with millions of supporters, we take on puppy mills, factory farms, the fur trade, trophy hunting, animal cosmetics testing and other cruel industries. We rescue and care for thousands of animals every year through our Animal Rescue Team’s work and other hands-on animal care services. We fight all forms of animal cruelty to achieve the vision behind our name: A humane society. </p></div>
     <div class="resourceLocation"><p>Washington, DC</p></div>
-    <div class="resourceSite"><p><a href="humanesociety.org">The Humane Society</a></p></div>
+    <div class="resourceSite"><p><a href="https://www.humanesociety.org">The Humane Society</a></p></div>
 </div>
 <div class="resourceContainer">
     <div class="resourceName"><p>Maddie's Fund</p></div>
@@ -57,7 +57,7 @@
         The Foundation has awarded nearly $250 million in grants toward increased community lifesaving, pioneering shelter medicine education and establishing foster care as a standard across the U.S.
         Maddie’s Fund proudly offers the industry a national voice, important funding opportunities for bold ideas, learning resources and access to collaborate and share lifesaving ideas. The Foundation invests its resources in a commitment to keeping pets and people together, creating a safety net of care for animals in need and operating within a culture of inclusiveness and humility.</p></div>
     <div class="resourceLocation"><p>Pleasonton, CA</p></div>
-    <div class="resourceSite"><p><a href=""maddiesfund.org>Maddie's Fund</a></p></div>
+    <div class="resourceSite"><p><a href="https://www.maddiesfund.org">Maddie's Fund</a></p></div>
 </div>
 <!--Template for Resource Page
 <div class="resourceContainer">

69cd82fc58d00486161d0147393c1bceb5af0a8e
HugoRad <62400330+HugoRad@users.noreply.github.com>
2021-03-08 11:54:21 -0600

    Update index.php

--- html/index.php
+++ html/index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -24,44 +23,34 @@
 -->
 
 <style>
-
-
-
+    
+    
 </style>
 </head>
 <body>
+    
+    <!--Header/navbar-->
 
-<div style = "position:fixed;top:0%;height:6%;left:0%;width:99.8%;background-color:rgb(214, 214, 214);border:solid 1px blue;"><!-- Header  -->
-
-    <img src = "../Only_USE_THIS_THEME/doggoWEBKIT.png" style = "display:inline;float:left;width:auto;height:100%;">
-
-    <a href = "index.php"><button class = "headerBut">Home</button></a> <!-- fill in your url to the page -->
-
-    <img src = "../Only_USE_THIS_THEME/kittyWEBKIT.jpg" style = "display:inline;float:left;width:auto;height:100%;">
-
-    <a href = "contactUs.php"><button class = "headerBut">Contact Us</button></a> <!-- fill in your url to the page -->
-
-    <img src = "../Only_USE_THIS_THEME/iguanaWEBKIT.png" style = "display:inline;;float:left;width:auto;height:100%;">
-
-    <a href = "TrendingPetNames.php"><button class = "headerBut">Trending</button></a> <!-- fill in your url to the page -->
-
-    <img src = "../Only_USE_THIS_THEME/fishWEBKIT.png" style = "display:inline;;float:left;width:auto;height:100%;">
-
-
-
-</div>
-
-
+    <div class="navbarMain">
+        <div class="navbarDog"><img src = "images/doggoWEBKIT.png" ></div>
+        <div class="navbarIndex"><a href = "index.php">Home</a> <!-- fill in your url to the page --></div>
+        <div class="navbarCat"><img src = "images/kittyWEBKIT.jpg" ></div>
+        <div class="navbarResources"><a href = "resources.html">Resources</a> <!-- fill in your url to the page --></div>
+        <div class="navbarIguana"><img src = "images/iguanaWEBKIT.png"></div>
+        <div class="navbarTrending"><a href = "TrendingPetNames.php">Trending</a> <!-- fill in your url to the page --></div>
+        <div class="navbarFish"><img src = "images/fishWEBKIT.png" ></div>
+        <div class="navbarContact"><a href = "ContactUs.php">Contact Us</a> <!-- fill in your url to the page --></div>
+    </div> 
+    
+    <!--END Header/navbar-->
+    
 <div id = "centerQuiz">
     <h3 id = "centerPageHeader"></h3>  <!-- Page Heading Here -->
-
  
 <!-- can put main stuff here, make div larger by editing universal style the id called centerPageHeader  -->
-
 <div class="main">
 	<div class='box1'>
 		<h2 #top>Take the pet name quiz!</h2>
-
 	</div>
 	
 	<h1></h1>
@@ -69,32 +58,38 @@
 		<ul id="choices"></ul>
 	<div class="box3">
 		<button id="back">Back</button>
+        <button id="newname">Get New Name</button>
 	</div>
 	
 	</div>
 	
 </div>
-
 </div>
-
-
-
+    
   
-
-
-<div style = "position:fixed;left:0%;bottom:0%;width:100%;height:15%;background-color:rgb(214, 214, 214);border:solid 1px blue;"><!-- footer  -->
-
-        <h3 style = "position:absolute;left:5%;top:2%;">We are committed to naming pets</h3>
-        <p style = "position:absolute;left:5%;top:26%;width:15%;">Feel free to take our quiz and use the results to name your awesome pet.  Leave us some feedback about how great our web site is!<p> 
-
-        <a href = "https://www.facebook.com"> <img src = "../Only_USE_THIS_THEME/fb.jpg" style = "position:absolute;left:41%;top:20%;width:2%;height:20%;"></a>
-        <a href = "https://www.instagram.com"> <img src = "../Only_USE_THIS_THEME/ig.jpg" style = "position:absolute;left:45%;top:20%;width:2%;height:20%;"></a>
-        <a href = "https://www.pinterest.com"> <img src = "../Only_USE_THIS_THEME/pn.jpg" style = "position:absolute;left:49%;top:20%;width:2%;height:20%;"></a>
-        <a href = "https://www.reddit.com"> <img src = "../Only_USE_THIS_THEME/rd.jpg" style = "position:absolute;left:53%;top:20%;width:2%;height:20%;"></a>
-        <a href = "https://www.tiktok.com"> <img src = "../Only_USE_THIS_THEME/tt.jpg" style = "position:absolute;left:57%;top:20%;width:2%;height:20%;"></a>
-        <a href = "https://www.twitter.com"> <img src = "../Only_USE_THIS_THEME/tw.jpg" style = "position:absolute;left:61%;top:20%;width:2%;height:20%;"></a>
-        <h3 style = "position:absolute;left:50%;bottom:20%;">Share Us!</h3>
-
-
+<!--Footer that is 3 boxes on top and 3 boxes on bottom in order 1, 2, 3. Some not used.-->
+<div class="footerMain">
+    <div class="footerTop1"><h3 >We are committed to naming pets</h3>
+        <p >Feel free to take our quiz and use the results to name your awesome pet.  Leave us some feedback about how great our web site is!<p></p>
+    </div>
+    <div class="footerTop2">
+        <a href = "https://www.facebook.com"> <img src = "images/fb.jpg" ></a>
+    <a href = "https://www.instagram.com"> <img src = "images/ig.jpg"></a>
+    <a href = "https://www.pinterest.com"> <img src = "images/pn.jpg" ></a>
+    <a href = "https://www.reddit.com"> <img src = "images/rd.jpg" ></a>
+    <a href = "https://www.tiktok.com"> <img src = "images/tt.jpg" ></a>
+    <a href = "https://www.twitter.com"> <img src = "images/tw.jpg" ></a>
+    </div>
+    <div class="footerTop3"></div><!--Placeholder for top right side of footer-->
+    <div class="footerBottom1"> </div><!--Placeholder for bottom left side of footer-->
+    <div class="footerBottom2"><h3 >Share Us!</h3></div>
+    <div class="footerBottom3"></div><!--Placeholder for bottom right side of footer-->
+</div>
+    <!--END footer-->
+    
+    
+    
+    
+    
 </body>
 </html>

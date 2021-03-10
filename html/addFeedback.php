c3da739a9ec274cc816ddfe65840daec7bc119d0
jakethorson <77747904+jakethorson@users.noreply.github.com>
2021-03-10 07:36:17 -0600

    Create addFeedback.php

---
+++ html/addFeedback.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html>
+<head>
+</head>
+
+<body>
+    <?php 
+
+        $fname = $_GET['fname'];
+        $email = $_GET['email'];
+		$phone = $_GET['phone'];
+		$response = $_GET['response'];
+
+		$servername = "localhost";  // connect to an already existing database if there is one
+		$username = "root";
+		$password = "";
+		$dbname = "nameurpet";
+
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
+
+
+		$sqli = "select * from feedbacks";
+        $index = 0;
+		$result = $conn->query($sqli);   
+		if ($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+                $index++;
+            }
+
+            $index++;
+            echo $index;
+        }else {
+			echo "Error: " . $sql . "<br>" . $conn->error;
+		} 
+
+		$sql = "insert into feedbacks (email, id, message, name, phone) values ('$email','$index','$response','$fname','$phone');";
+				if ($conn->query($sql) == TRUE) {
+					echo "New record created successfully";
+				} else {
+					echo "Error: " . $sql . "<br>" . $conn->error;
+				}
+
+		$conn->close();
+	?>
+</body>
+</html>

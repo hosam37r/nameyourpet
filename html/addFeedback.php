c89699c5244128aa9f55510c6859511e93f51ef8
jakethorson <77747904+jakethorson@users.noreply.github.com>
2021-04-03 12:53:31 -0500

    Update addFeedback.php

--- html/addFeedback.php
+++ html/addFeedback.php
@@ -14,7 +14,7 @@
 		$servername = "127.0.0.1";  
 		$username = "root";
 		$password = "";
-		$dbname = "nameyourpet";
+		$dbname = "feedbacks";
 
 		$conn = new mysqli($servername, $username, $password, $dbname);
 		if ($conn->connect_error) {

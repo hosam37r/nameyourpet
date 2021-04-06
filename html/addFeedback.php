cdb0f2d2a7d4d9a92f16422ab52fc75e510d692d
jakethorson <77747904+jakethorson@users.noreply.github.com>
2021-04-06 08:15:06 -0500

    Update addFeedback.php

--- html/addFeedback.php
+++ html/addFeedback.php
@@ -20,7 +20,7 @@
 		if ($conn->connect_error) {
 			die("Connection failed: " . $conn->connect_error);
 		}
-
+		ini_set('display_errors', '1'); ini_set('display_startup_errors', '1'); error_reporting(E_ALL);
 
 		$sqli = "select * from feedbacks";
         $index = 0;

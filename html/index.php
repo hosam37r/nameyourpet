fac0939ade1f5b8a40ce614186a2b1c6c200b8e6
radnee <75645533+radnee@users.noreply.github.com>
2021-04-18 10:33:45 -0500

    Update form method to Post

--- html/index.php
+++ html/index.php
@@ -1,37 +1,29 @@
 <!DOCTYPE html>
 <html>
 <head>
-    
+
     <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
-	<script type="text/javascript" src="script.js"></script>
-    
+    <script type="text/javascript" src="script.js"></script>
 
-<!-- Fonts -->
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
 
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
 
-<!-- cssPage -->
-<link rel="stylesheet" href="universalStyle.css">
+    <!-- cssPage -->
+    <link rel="stylesheet" href="universalStyle.css">
 
-
-
-<!--
+    <!--
     All the images should be put into a seperate folder labelled "images"
--->
-
-<style>
-    
-    
-</style>
+    -->
 </head>
 <body>
-    
+
     <!--Header/navbar-->
 
-   <div class="navbarMain">
+    <div class="navbarMain">
 
         <div class="navbarDog"><img src = "images/dog.png" ></div>
 
@@ -50,60 +42,57 @@
         <div class="navbarContact"><a href = "contactUs.php">Contact Us</a> <!-- fill in your url to the page --></div>
 
     </div> 
-    
+
     <!--END Header/navbar-->
-    
-<div id = "centerQuiz">
-    <h3 id = "centerPageHeader"></h3>  <!-- Page Heading Here -->
- 
-<!-- can put main stuff here, make div larger by editing universal style the id called centerPageHeader  -->
-<div class="main">
-	<div class='box1'>
-		<h2 #top>Take the pet name quiz!</h2>
-	</div>
-	
-	<h1></h1>
-	<div class="box2">
-		<ul id="choices"></ul>
-        <div id="emailPrompt">
-            <form action="addEmail.php" method="get"> 
-						<input type="text" id="email" name="email" placeholder="email" style = ""><p></p>
+
+    <div id = "centerQuiz">
+        <h3 id = "centerPageHeader"></h3>  <!-- Page Heading Here -->
+
+        <!-- can put main stuff here, make div larger by editing universal style the id called centerPageHeader  -->
+        <div class="main">
+            <div class='box1'>
+                <h2 #top>Take the pet name quiz!</h2>
+            </div>
+
+            <h1></h1>
+            <div class="box2">
+                <ul id="choices"></ul>
+                <div id="emailPrompt">
+                    <form action="addEmail.php" method="POST"> 
+                        <input type="text" id="email" name="email" placeholder="email" style = ""><p></p>
                         <input id="submission" type="submit" value="Submit">
-							</form></div>
-	<div class="box3">
-		<button id="back">Back</button>
-        <button id="newname">Get New Name</button>
-	</div>
-	
-	</div>
-	
-</div>
-</div>
-    
-  
-<!--Footer that is 3 boxes on top and 3 boxes on bottom in order 1, 2, 3. Some not used.-->
-<div class="footerMain">
-    <div class="footerTop1"><h3 >We are committed to naming pets</h3>
-        <p >Feel free to take our quiz and use the results to name your awesome pet.  Leave us some feedback about how great our web site is!<p></p>
+                    </form>
+                </div>
+                <div class="box3">
+                    <button id="back">Back</button>
+                    <button id="newname">Get New Name</button>
+                </div>
+
+            </div>
+
+        </div>
     </div>
-    <div class="footerTop2">
-        <a href = "https://www.facebook.com"> <img src = "images/fb.jpg" ></a>
-    <a href = "https://www.instagram.com"> <img src = "images/ig.jpg"></a>
-    <a href = "https://www.pinterest.com"> <img src = "images/pn.jpg" ></a>
-    <a href = "https://www.reddit.com"> <img src = "images/rd.jpg" ></a>
-    <a href = "https://www.tiktok.com"> <img src = "images/tt.jpg" ></a>
-    <a href = "https://www.twitter.com"> <img src = "images/tw.jpg" ></a>
+
+
+    <!--Footer that is 3 boxes on top and 3 boxes on bottom in order 1, 2, 3. Some not used.-->
+    <div class="footerMain">
+        <div class="footerTop1"><h3 >We are committed to naming pets</h3>
+            <p >Feel free to take our quiz and use the results to name your awesome pet.  Leave us some feedback about how great our web site is!<p></p>
+        </div>
+        <div class="footerTop2">
+            <a href = "https://www.facebook.com"> <img src = "images/fb.jpg" ></a>
+            <a href = "https://www.instagram.com"> <img src = "images/ig.jpg"></a>
+            <a href = "https://www.pinterest.com"> <img src = "images/pn.jpg" ></a>
+            <a href = "https://www.reddit.com"> <img src = "images/rd.jpg" ></a>
+            <a href = "https://www.tiktok.com"> <img src = "images/tt.jpg" ></a>
+            <a href = "https://www.twitter.com"> <img src = "images/tw.jpg" ></a>
+        </div>
+        <div class="footerTop3"></div><!--Placeholder for top right side of footer-->
+        <div class="footerBottom1"> </div><!--Placeholder for bottom left side of footer-->
+        <div class="footerBottom2"><h3 >Share Us!</h3></div>
+        <div class="footerBottom3"></div><!--Placeholder for bottom right side of footer-->
     </div>
-    <div class="footerTop3"></div><!--Placeholder for top right side of footer-->
-    <div class="footerBottom1"> </div><!--Placeholder for bottom left side of footer-->
-    <div class="footerBottom2"><h3 >Share Us!</h3></div>
-    <div class="footerBottom3"></div><!--Placeholder for bottom right side of footer-->
-</div>
     <!--END footer-->
-    
-    
-    
-    
-    
+
 </body>
 </html>

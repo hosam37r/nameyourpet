7acf9dc4b0d694a1c0f6d64b05bc6d6d7acbc1d6
radnee <75645533+radnee@users.noreply.github.com>
2021-04-18 10:28:02 -0500

    Updated addEmail.php
    Check if email exists before adding to the database. Add json response object.

--- html/addEmail.php
+++ html/addEmail.php
@@ -1,26 +1,39 @@
 <?php
+	
+	/* Attempt MySQL server connection. Assuming you are running MySQL
+	server with default setting (user 'root' with no password) */
+	$link = mysqli_connect("localhost", "student", "password", "feedbacks2");
+	
+	// Check connection
+	if($link === false){
+		die("ERROR: Could not connect. " . mysqli_connect_error());
+	}
+	
+	// Escape user inputs for security
+	$email = mysqli_real_escape_string($link, $_POST['email']);
+	
+	// Check if email already exists in the database
+	$sql = "SELECT * FROM EMAIL WHERE EMAIL = '$email'";
+	$result = mysqli_query($link, $sql);
+	
+	if (mysqli_num_rows($result) > 0) {
+		$arr = array('success' => 1, 'message' => "Email already exists. Thank you!");
+	}
+	else {
+		// Attempt insert query execution
+		$sql = "INSERT INTO EMAIL (EMAIL) VALUES ('$email')";
 
-/* Attempt MySQL server connection. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-$link = mysqli_connect("localhost", "student", "password", "feedbacks2");
+		if(mysqli_query($link, $sql)){
+			$arr = array('success' => 1, 'message' => "Email added successfully.");
+		} else{
+			$arr = array('success' => 0, 'message' => "Could not execute SQL command.");
+		}
+	}
+	
+	// Close connection
+	mysqli_close($link);
+	
+	// Send response to javascript
+	echo json_encode($arr);
 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-
-// Escape user inputs for security
-$email = mysqli_real_escape_string($link, $_REQUEST['email']);
-
-// Attempt insert query execution
-$sql = "INSERT INTO email (email) VALUES ('$email')";
-if(mysqli_query($link, $sql)){
-    echo "Records added successfully.";
-} else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-}
-
-// Close connection
-mysqli_close($link);
-header("Location: index.php");
 ?>
